ee5fb67d206d1ecf3149bd7325fac3b99877b474
krishav234 <krishav234@gmail.com>
2023-12-30 20:31:26 +0530

    Add initial files

---
+++ src/api/api.js
@@ -0,0 +1,11 @@
+const apiKey = 'cab2777813f251f49f14a968a37a1569';
+
+const getWeather = async (city) => {
+    return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+    .then((res) => res.json())
+    .then((json) => {
+        return json;
+    })
+}
+
+export default getWeather;
